7518c0dfb32a808165764bd2490e332328586bb9
huangdihd <hd20100104@163.com>
2025-09-29 21:27:40 +0800

    Modify level of no plugin found logs. Modify copyright text.

--- src/main/java/xin/bbtt/mcbot/plugin/PluginManager.java
+++ src/main/java/xin/bbtt/mcbot/plugin/PluginManager.java
@@ -1,20 +1,19 @@
 
 /*
- * # Copyright (C) 2025 huangdihd
- * #
- * # This program is free software: you can redistribute it and/or modify
- * # it under the terms of the GNU General Public License as published by
- * # the Free Software Foundation, either version 3 of the License, or
- * # (at your option) any later version.
- * #
- * # This program is distributed in the hope that it will be useful,
- * # but WITHOUT ANY WARRANTY; without even the implied warranty of
- * # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * # GNU General Public License for more details.
- * #
- * # You should have received a copy of the GNU General Public License
- * # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *   Copyright (C) 2024-2025 huangdihd
  *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package xin.bbtt.mcbot.plugin;
@@ -89,7 +88,7 @@ public class PluginManager {
 
         File[] files = dir.listFiles((dir1, name) -> name.endsWith(".jar"));
         if (files == null || files.length == 0) {
-            log.error("No plugins found.");
+            log.info("No plugins found.");
             return;
         }
 
