c69335ec52dc6a384acf1763736e138f176610bb
huangdihd <hd20100104@163.com>
2025-12-25 00:45:26 +0800

    Refactor ReflectiveEventExecutor and enhance packet handling in PacketListener

--- src/main/java/xin/bbtt/mcbot/listeners/PacketListener.java
+++ src/main/java/xin/bbtt/mcbot/listeners/PacketListener.java
@@ -3,13 +3,19 @@ package xin.bbtt.mcbot.listeners;
 import org.geysermc.mcprotocollib.network.Session;
 import org.geysermc.mcprotocollib.network.event.session.SessionAdapter;
 import org.geysermc.mcprotocollib.network.packet.Packet;
+import org.geysermc.mcprotocollib.protocol.codec.MinecraftPacket;
 import xin.bbtt.mcbot.Bot;
 import xin.bbtt.mcbot.events.ReceivePacketEvent;
 
 public class PacketListener extends SessionAdapter {
     @Override
     public void packetReceived(Session session, Packet packet) {
-        ReceivePacketEvent<? extends Packet> event = new ReceivePacketEvent<>(packet);
+        if (!(packet instanceof MinecraftPacket minecraftPacket)) return;
+
+        ReceivePacketEvent<?> event = new ReceivePacketEvent<>(
+            minecraftPacket,
+            minecraftPacket.getClass()
+        );
         Bot.Instance.getPluginManager().events().callEvent(event);
     }
 }
