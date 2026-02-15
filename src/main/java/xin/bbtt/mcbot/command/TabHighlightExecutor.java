a81611199ac2a0dc706a735883210902841c4801
huangdihd <hd20100104@163.com>
2026-02-15 10:13:19 +0800

    Add HighlightExecutor and TabHighlightExecutor for JLine command highlighting

---
+++ src/main/java/xin/bbtt/mcbot/command/TabHighlightExecutor.java
@@ -0,0 +1,27 @@
+/*
+ *   Copyright (C) 2026 huangdihd
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package xin.bbtt.mcbot.command;
+
+import org.jline.utils.AttributedString;
+
+import java.util.List;
+
+public abstract class TabHighlightExecutor extends CommandExecutor {
+    public abstract List<String> onTabComplete(Command cmd, String label, String[] args);
+    public abstract AttributedString onHighlight(Command cmd, String label, String[] args);
+}
